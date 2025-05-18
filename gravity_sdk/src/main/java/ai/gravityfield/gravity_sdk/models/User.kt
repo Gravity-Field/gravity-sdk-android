e36ea15052b985dd3f7e11270593062c1cf36db2
Sergei <sergflutterdev@gmail.com>
2025-05-18 08:19:26 +0500

    Change user attributes values to string

--- gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/User.kt
+++ gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/User.kt
@@ -1,14 +1,13 @@
 package ai.gravityfield.gravity_sdk.models
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonElement
 
 @Serializable
 data class User(
     val uid: String? = null,
     val custom: String? = null,
     val ses: String? = null,
-    val attributes: Map<String, JsonElement>? = null
+    val attributes: Map<String, String>? = null
 ) {
     companion object {
         fun fromJson(json: Map<String, Any?>): User {
