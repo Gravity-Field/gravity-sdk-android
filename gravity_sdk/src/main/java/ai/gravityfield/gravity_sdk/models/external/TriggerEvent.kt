31d962fd8473e59e1b47adc3c7764d72241af1da
Sergei <sergflutterdev@gmail.com>
2025-05-18 09:38:35 +0500

    Add trigger events

--- gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/external/TriggerEvent.kt
+++ gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/external/TriggerEvent.kt
@@ -21,9 +21,70 @@ class AddToCartEvent(
 }
 
 @Serializable
+class PurchaseEvent(
+    val uniqueTransactionId: String,
+    val value: Double,
+    val currency: String?,
+    val cart: List<CartItem>?,
+) : TriggerEvent() {
+    override val type: String = "purchase-v1"
+    override val name: String = "Purchase"
+}
+
+@Serializable
+class RemoveFromCartEvent(
+    val value: Double,
+    val productId: String,
+    val quantity: Int,
+    val currency: String?,
+    val cart: List<CartItem>?,
+) : TriggerEvent() {
+    override val type: String = "remove-from-cart-v1"
+    override val name: String = "Remove from Cart"
+}
+
+@Serializable
+class SyncCartEvent(
+    val currency: String?,
+    val cart: List<CartItem>?,
+) : TriggerEvent() {
+    override val type: String = "sync-cart-v1"
+    override val name: String = "Sync cart"
+}
+
+@Serializable
+class AddToWishlistEvent(
+    val productId: String,
+) : TriggerEvent() {
+    override val type: String = "add-to-wishlist-v1"
+    override val name: String = "Add to Wishlist"
+}
+
+@Serializable
+class SingUpEvent(
+    val hashedEmail: String,
+    val cuid: String,
+    val cuidType: String
+) : TriggerEvent() {
+    override val type: String = "signup-v1"
+    override val name: String = "Signup"
+}
+
+@Serializable
+class LoginEvent(
+    val hashedEmail: String,
+    val cuid: String,
+    val cuidType: String
+) : TriggerEvent() {
+    override val type: String = "login-v1"
+    override val name: String = "Login"
+}
+
+@Serializable
 class CustomEvent(
     override val type: String,
-    override val name: String
+    override val name: String,
+    val properties: Map<String, String>? = null
 ) : TriggerEvent()
 
 @Serializable
