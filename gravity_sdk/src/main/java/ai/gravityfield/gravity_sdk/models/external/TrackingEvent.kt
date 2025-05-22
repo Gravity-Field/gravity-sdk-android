524bcdb9aeeee58b217762aba582f917a645d9b9
Sergei <sergflutterdev@gmail.com>
2025-05-22 07:31:32 +0500

    Add TrackingEvent

---
+++ gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/external/TrackingEvent.kt
@@ -0,0 +1,47 @@
+package ai.gravityfield.gravity_sdk.models.external
+
+import ai.gravityfield.gravity_sdk.models.CampaignContent
+import ai.gravityfield.gravity_sdk.models.Slot
+import ai.gravityfield.gravity_sdk.network.Campaign
+
+sealed class TrackingEvent(val campaign: Campaign)
+
+class ContentLoadEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class ContentImpressionEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class ContentVisibleImpressionEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class ContentCloseEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class CopyEvent(
+    val copiedValue: String, val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class CancelEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class FollowUrlEvent(
+    val url: String, val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class FollowDeeplinkEvent(
+    val deeplink: String, val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class RequestPushEvent(
+    val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
+
+class ProductImpressionEvent(
+    val slot: Slot, val content: CampaignContent, campaign: Campaign
+) : TrackingEvent(campaign)
