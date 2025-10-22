21b9f56c7674286abc12727d48bc19c84aa070a7
Sergei <sergflutterdev@gmail.com>
2025-10-22 21:06:52 +0500

    Fix user agent field name

--- gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/Device.kt
+++ gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/models/Device.kt
@@ -1,9 +1,11 @@
 package ai.gravityfield.gravity_sdk.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Device(
+    @SerialName("ua")
     val userAgent: String,
     val id: String?,
     val tracking: String?,
