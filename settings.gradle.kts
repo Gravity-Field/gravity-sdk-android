160545209af4fdea15848875814d72c791c59abd
Sergei <sergflutterdev@gmail.com>
2026-01-27 15:38:34 +0500

    Add local repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("gravity_sdk/repository") }
     }
     versionCatalogs {
         create("gravitySdk") {
