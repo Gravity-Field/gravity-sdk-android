0bbb87064c462b6876027effa139bab8fe852789
Sergei <sergflutterdev@gmail.com>
2026-02-28 18:41:23 +0500

    Fix CloseButton

--- gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/ui/CloseButton.kt
+++ gravity_sdk/src/main/java/ai/gravityfield/gravity_sdk/ui/CloseButton.kt
@@ -1,16 +1,25 @@
 package ai.gravityfield.gravity_sdk.ui
 
 import ai.gravityfield.gravity_sdk.extensions.absolutePosition
+import ai.gravityfield.gravity_sdk.extensions.conditional
 import ai.gravityfield.gravity_sdk.models.Close
 import ai.gravityfield.gravity_sdk.models.OnClickModel
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 
@@ -19,31 +28,57 @@ internal fun CloseButton(
     close: Close,
     onClickCallback: (model: OnClickModel) -> Unit,
 ) {
+    val image = close.image
+    val style = close.style
+    val positioned = style.positioned
+    val width = style.size?.width
+    val height = style.size?.height
+    val contentScale = style.fit ?: ContentScale.Crop
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
     Box(
         modifier = Modifier
             .absolutePosition(
-                left = close.style?.positioned?.left?.dp,
-                top = close.style?.positioned?.top?.dp,
-                right = close.style?.positioned?.right?.dp,
-                bottom = close.style?.positioned?.bottom?.dp,
+                left = positioned?.left?.dp,
+                top = positioned?.top?.dp,
+                right = positioned?.right?.dp,
+                bottom = positioned?.bottom?.dp,
+            )
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = {
+                    if (close.onClick != null) onClickCallback.invoke(close.onClick)
+                }
+            )
+            .then(
+                if (isPressed) {
+                    Modifier.alpha(0.5f)
+                } else {
+                    Modifier
+                }
             )
     ) {
-        IconButton(
-            onClick = {
-                if (close.onClick != null) onClickCallback.invoke(close.onClick)
-            }
-        ) {
-            if (close.image != null) {
-                Image(
-                    painter = rememberAsyncImagePainter(model = close.image),
-                    contentDescription = null,
-                )
-            } else {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = null,
-                )
-            }
+        if (image != null) {
+            Image(
+                modifier = Modifier
+                    .conditional(width != null) {
+                        width(width!!.dp)
+                    }
+                    .conditional(height != null) {
+                        height(height!!.dp)
+                    },
+                painter = rememberAsyncImagePainter(model = image),
+                contentDescription = null,
+                contentScale = contentScale
+            )
+        } else {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = null,
+            )
         }
     }
 }
